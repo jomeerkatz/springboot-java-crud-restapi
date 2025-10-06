0afa2b791f7a058e5cf558d6e65d852d1e0164f5
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-10-06 11:54:53 +0200

    add RestController Annotation

--- src/main/java/com/example/connectToPostgres/controllers/BookController.java
+++ src/main/java/com/example/connectToPostgres/controllers/BookController.java
@@ -10,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BookController {
 
     private Mapper<BookEntity, BookDto> bookMapper;
